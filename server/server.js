57510ddfc6cb89dc2c97659b5394ba8e16a1f40a
JUIC3zxy <juleszxy@gmail.com>
2025-01-06 17:13:43 +1100

    cahnge port

--- server/server.js
+++ server/server.js
@@ -44,6 +44,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3003, () => {
-  console.log("server is running on 3003");
+server.listen(PORT, () => {
+  console.log(`server is running on ${PORT}`);
 });
